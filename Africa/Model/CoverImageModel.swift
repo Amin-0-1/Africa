ff60f6533fe7af275d33b284d0069046aed863cb
Amin <mahmoudamin373@gmail.com>
2023-08-31 00:14:34 +0300

    First Screen
    * finished first screen

---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Amin on 30/08/2023.
+//
+
+import SwiftUI
+
+struct CoverImage:Codable,Identifiable{
+    let id:Int
+    let name:String
+}
