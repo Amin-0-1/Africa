f2f7f75e247720162f0eb6aa5ab5d12fb750366b
Amin <mahmoudamin373@gmail.com>
2023-10-31 16:45:27 +0200

    gallery
    - add gallery slider
    - add gallery option in toolbar

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,28 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     let animals:[Animal] = Bundle.main.decode("animals.json")
-    
+    let haptics = UIImpactFeedbackGenerator(style: .soft)
+    @State private var isGridViewActive = false
     
     var body: some View {
         NavigationView {
-            List{
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                
-
-                ForEach(animals) { animal in
-                    
-                    NavigationLink {
-                        AnimalDetailView(animal: animal)
-                    } label: {
-                        AnimalListItemView(animal: animal)
+            Group{
+                if !isGridViewActive {
+                    List{
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            
+                            NavigationLink {
+                                AnimalDetailView(animal: animal)
+                            } label: {
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
                     }
+                } else {
+                    Text("show grid")
                 }
             }
             .listStyle(.plain)
             .navigationTitle("Africa")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    HStack(spacing: 15) {
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }, label: {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                        })
+                        .font(.title2)
+                        .foregroundStyle(isGridViewActive ? .primary : Color.accentColor)
+                        
+                        Button(action: {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }, label: {
+                            Image(systemName: "square.grid.2x2")
+                        })
+                        .font(.title2)
+                        .foregroundStyle(isGridViewActive ? Color.accentColor : .primary )
+                    }
+                }
+            }
         }
     }
 }
--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -9,18 +9,20 @@ import SwiftUI
 
 struct GalleryView: View {
     @State private var selectedAnimal:String = "lion"
+    @State private var sliderValue:Float = 0.5
+    private var haptic:UIImpactFeedbackGenerator = .init(style: .soft)
     let animals:[Animal] = Bundle.main.decode("animals.json")
-    
-    // MARK: - Simple grid defination
-   //let gridLayout : [GridItem] = [
-   //    GridItem(.flexible()),
-   //    GridItem(.flexible()),
-   //    GridItem(.flexible()),
-   //]
+
     
     // MARK: - Effecient grid defination
-    let gridLayout:[GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // let gridLayout:[GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    // MARK: - dynamic grid
+    @State private var gridLayout:[GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
     
+    func gridSwitch(){
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
     var body: some View {
         ScrollView(.vertical, showsIndicators: false){
             // MARK: - Grid
@@ -31,6 +33,13 @@ struct GalleryView: View {
                     .clipShape(Circle())
                     .overlay(Circle().stroke(Color.white,lineWidth: 8))
                 
+                Slider(value: $gridColumn, in: 2...4,step:1)
+                    .onChange(of: gridColumn, perform: { value in
+                        withAnimation {
+                            gridSwitch()
+                        }
+                    })
+                
                 LazyVGrid(columns: gridLayout,spacing: 10) {
                     ForEach(animals) { item in
                         Image(item.image)
@@ -39,10 +48,14 @@ struct GalleryView: View {
                             .clipShape(Circle())
                             .overlay(Circle().stroke(.white,lineWidth: 1))
                             .onTapGesture {
+                                haptic.impactOccurred()
                                 selectedAnimal = item.image
                             }
                     }
                 }
+                .onAppear(perform: {
+                    gridSwitch()
+                })
             }
             .padding(.horizontal,10)
             .padding(.vertical,50)
